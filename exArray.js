5bd52adc4a70f4035fd7c72311c1876c10d270cd
Hycaro Phillype <hycarophillype0@gmail.com>
2024-02-20 10:50:22 -0300

    Primeiro commit: Adinando Exercico finalizado

---
+++ exArray.js
@@ -0,0 +1,14 @@
+let listaGenerica = [];
+
+let listaDeProgamacao = ["JavaScript", "C", "C++", "Kotlin", "Python"];
+
+listaDeProgamacao.push("Java", "Ruby", "Golang");
+
+// exibir primeiro elemento
+console.log(listaDeProgamacao[0]);
+
+// exibir o segundo elemento
+console.log(listaDeProgamacao[1]);
+
+// exibir o ultimo elemento
+console.log(listaDeProgamacao.length - 1);
